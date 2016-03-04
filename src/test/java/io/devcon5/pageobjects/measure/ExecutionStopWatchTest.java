5fb08f97ef02dccd5aa3221f1c3d423b5a10f015
Gerald Mücke <gerald@inkstand.io>
2016-03-04 21:21:12 +0100

    DC5PO-3 added unit tests

--- src/test/java/io/devcon5/pageobjects/measure/ExecutionStopWatchTest.java
+++ src/test/java/io/devcon5/pageobjects/measure/ExecutionStopWatchTest.java
@@ -1,4 +1,20 @@
-package io.devcon5.pageobjects;
+/*
+ * Copyright 2015-2016 DevCon5 GmbH, info@devcon5.ch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.devcon5.pageobjects.measure;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -6,8 +22,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.time.Duration;
 
-import io.devcon5.pageobjects.measure.ExecutionStopWatch;
-import io.devcon5.pageobjects.measure.MeasuredExecutionResult;
 import org.junit.Test;
 
 /**
